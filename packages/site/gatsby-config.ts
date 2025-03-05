3c4d02236ec917e645339da91c78ce6d21e7abad
Edinaldo Junior <epsjunior@icloud.com>
2025-03-05 03:29:01 -0300

    fix: lint

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -4,10 +4,7 @@ const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
   jsxRuntime: 'automatic',
 
-  plugins: [
-    'gatsby-plugin-svgr',
-    'gatsby-plugin-styled-components'
-  ],
+  plugins: ['gatsby-plugin-svgr', 'gatsby-plugin-styled-components'],
 };
 
 export default config;
